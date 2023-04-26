1de3f951a7568861c36937a0dc239b5a7fa3e5f5
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-26 19:28:40 +0530

    2

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default function Home() {
 
-    const [users, setusers] = useState([])
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
         // console.log("Code with Kishore.");
@@ -13,7 +13,7 @@ export default function Home() {
     const loadUsers = async () => {
         const result = await axios.get("http://localhost:8080/users")
         // console.log(result);
-        console.log(result);
+        console.log(result.data);
     }
 
     return (
